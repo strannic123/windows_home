b3c2c4b94a362914605080cde76d3b73bcf3f54b
strannic123 <manager350@mail.ru>
2020-09-02 18:17:27 +0300

    add popup images

---
+++ src/js/modules/images.js
@@ -0,0 +1,30 @@
+const images = () => {
+    const imagePopup = document.createElement('div'),
+          workSection = document.querySelector('.works'),
+          bigImg = document.createElement('img');
+
+    imagePopup.classList.add('popup');
+    workSection.appendChild(imagePopup);
+
+    imagePopup.style.justifyContent = 'center';
+    imagePopup.style.alignContent = 'center';
+    imagePopup.style.display = 'none';
+
+    imagePopup.appendChild(bigImg);
+
+    workSection.addEventListener('click', (e) => {
+        e.preventDefault();
+        let target = e.target;
+        if (target && target.classList.contains('preview')) {
+            imagePopup.style.display = 'flex';
+            const path = target.parentNode.getAttribute('href');
+            bigImg.setAttribute('src', path);
+        }
+
+        if(target && target.matches('div.popup')) {
+            imagePopup.style.display = 'none';
+        }
+    })
+};
+
+export default images;
